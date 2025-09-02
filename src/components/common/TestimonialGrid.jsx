3a833045237ae5c207a329b2a8b63fc68286aae7
Irfan Ahamed S <irfan.ahamed@demanzo.com>
2025-09-02 18:54:08 +0530

    Added Reviews

--- src/components/common/TestimonialGrid.jsx
+++ src/components/common/TestimonialGrid.jsx
@@ -1,29 +1,74 @@
 import React from "react";
 
 const testimonials = [
+  //  {
+  //   username: "suryakant arora",
+  //   text: "​I had an incredible experience working with Crezeal Technologies. Their expertise in web development is unmatched.The team is professional, highly skilled, and always ready to go the extra mile to ensure client satisfaction.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Karthik Krishnan",
+  //   text: "After searching and dealing with different designers and developers without success, I finally found Crezeal Technologies.The web designers team at Crezeal is the best! Couldn't be more happy with the quality website they developed for me.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Print Cord",
+  //   text: "I recently had the pleasure of working with Crezeal Technologies to develop my website, PrintCord.in, and I couldn’t be happier with the results! Crezeal Technologies impressed me from the start with their professionalism, technical expertise, and dedication to bringing my vision to life.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "shanu J",
+  //   text: "Crezeal is the best web designing company. I absolutely loved working with the team in creating a unique website for my Bakery. They brought so many great ideas and design with user experience points to the table and was able to construct what our vision was for our businesses into a stunning website!!I highly recommend Crezeal Technologies. If you are looking for a affordable professional service this is the best around Nagercoil!!!",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Ayush Kokra",
+  //   text: "Crezeal helped me with the best customization of the website alongside with the cooperative team. The developers know what they are doing, I am very satisfied with my website as the developers are also knowledgeable about work and the field. I got no chance of complaints against them.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Godson Chinnu",
+  //   text: "Working with Crezeal has been a game-changer for our business. Our business was doing well with offline but we felt that its time to build online. We approached Crezeal Benujah guided us with the plans. They have build me a beautiful website. Their team has significantly boosted our brand visibility. The best part is that I am receiving a couple of calls every week from new customers. Thanks team.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Sree ganapathy",
+  //   text: "I recently had an amazing experience with Crezeal for website design & digital marketing. Their service truly impressed me simply because they attentively & patiently listened to our lengthy brief. And everything worked successfully and now we receive enquiries regularly. Highly recommended.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Krishna Moorthy",
+  //   text: "I recently worked with Crezeal for our website development and am very happy with the results. Special thanks to Benujah for her excellent support. I highly recommend Crezeal for website development 👌👌",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Rajasozhaperumal G",
+  //   text: "So lucky to have found Benujah! She explained the services so I could understand what was possible. I am very happy with their website designs and digital marketing which are bringing great results so far! Great team to work with.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "hari perumal",
+  //   text: "Working at Demanzo has been a highly rewarding experience. The company offers a collaborative and creative environment where team members are encouraged to grow, learn, and contribute meaningfully.I’ve had the opportunity to work on exciting digital projects with a talented team that values innovation and results. The leadership is supportive, communicative, and genuinely cares about employee development.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Dominic Rosario",
+  //   text: "It was a fantastic experience. Their team is highly customer-centric, ensuring that every interaction is tailored to meet your needs, also they respond quick on time and genuine commitment to providing top-notch service.",
+  //   stars: 5,
+  // },
+  // {
+  //   username: "Kevin Perdigues",
+  //   text: "Very good quality-price ratio. Jude is fair and his team are magicians of the web!Great team to work with. They've worked hard to pull off a fantastic result beyond my expectations.",
+  //   stars: 4,
+  // },
   {
-    username: "suryakant arora",
-    text: "​I had an incredible experience working with Crezeal Technologies. Their expertise in web development is unmatched.The team is professional, highly skilled, and always ready to go the extra mile to ensure client satisfaction.",
-    stars: 5,
-  },
-  {
-    username: "Karthik Krishnan",
-    text: "After searching and dealing with different designers and developers without success, I finally found Crezeal Technologies.The web designers team at Crezeal is the best! Couldn't be more happy with the quality website they developed for me.",
-    stars: 5,
-  },
-  {
-    username: "Print Cord",
-    text: "I recently had the pleasure of working with Crezeal Technologies to develop my website, PrintCord.in, and I couldn’t be happier with the results! Crezeal Technologies impressed me from the start with their professionalism, technical expertise, and dedication to bringing my vision to life.",
-    stars: 5,
-  },
-  {
-    username: "shanu J",
+    username: "Shanu J",
     text: "Crezeal is the best web designing company. I absolutely loved working with the team in creating a unique website for my Bakery. They brought so many great ideas and design with user experience points to the table and was able to construct what our vision was for our businesses into a stunning website!!I highly recommend Crezeal Technologies. If you are looking for a affordable professional service this is the best around Nagercoil!!!",
     stars: 5,
   },
   {
-    username: "Ayush Kokra",
-    text: "Crezeal helped me with the best customization of the website alongside with the cooperative team. The developers know what they are doing, I am very satisfied with my website as the developers are also knowledgeable about work and the field. I got no chance of complaints against them.",
+    username: "Suryakant Arora",
+    text: "​I had an incredible experience working with Crezeal Technologies. Their expertise in web development is unmatched.The team is professional, highly skilled, and always ready to go the extra mile to ensure client satisfaction. Communication was seamless throughout the project, and they completed everything on time and within budget. I highly recommend ​Crezeal Technologies to anyone looking for top-notch web development ​. Thank you for your amazing work! 👏✨",
     stars: 5,
   },
   {
@@ -32,7 +77,22 @@ const testimonials = [
     stars: 5,
   },
   {
-    username: "Sree ganapathy",
+    username: "Print Cord",
+    text: "I recently had the pleasure of working with Crezeal Technologies to develop my website, PrintCord.in, and I couldn’t be happier with the results! Crezeal Technologies impressed me from the start with their professionalism, technical expertise, and dedication to bringing my vision to life. The site is not only visually appealing but also highly functional and optimized for both desktop and mobile. I highly recommend Crezeal Technologies to anyone looking for a reliable, innovative web development partner!",
+    stars: 5,
+  },
+  {
+    username: "A One Photography",
+    text: "Good digital marketing and website development company in Nagercoil. Very polite customer support. Highly recommended",
+    stars: 5,
+  },
+  {
+    username: "Aiswerya Kailash",
+    text: "Got a chance to work along with them, it was a great experience. Probably one of the best digital marketing company to level up your business.",
+    stars: 5,
+  },
+  {
+    username: "Sree Ganapathy",
     text: "I recently had an amazing experience with Crezeal for website design & digital marketing. Their service truly impressed me simply because they attentively & patiently listened to our lengthy brief. And everything worked successfully and now we receive enquiries regularly. Highly recommended.",
     stars: 5,
   },
@@ -42,24 +102,24 @@ const testimonials = [
     stars: 5,
   },
   {
+    username: "Crest Ronics",
+    text: "One of the best and finest digital marketing company around Nagercoil. Crezeal team provide valuable services for the payment we provided. Thank Benuja and team for the great service. Thank you",
+    stars: 5,
+  },
+  {
     username: "Rajasozhaperumal G",
     text: "So lucky to have found Benujah! She explained the services so I could understand what was possible. I am very happy with their website designs and digital marketing which are bringing great results so far! Great team to work with.",
     stars: 5,
   },
   {
-    username: "hari perumal",
-    text: "Working at Demanzo has been a highly rewarding experience. The company offers a collaborative and creative environment where team members are encouraged to grow, learn, and contribute meaningfully.I’ve had the opportunity to work on exciting digital projects with a talented team that values innovation and results. The leadership is supportive, communicative, and genuinely cares about employee development.",
-    stars: 5,
-  },
-  {
-    username: "Dominic Rosario",
-    text: "It was a fantastic experience. Their team is highly customer-centric, ensuring that every interaction is tailored to meet your needs, also they respond quick on time and genuine commitment to providing top-notch service.",
-    stars: 5,
-  },
-  {
     username: "Kevin Perdigues",
-    text: "Very good quality-price ratio. Jude is fair and his team are magicians of the web!Great team to work with. They've worked hard to pull off a fantastic result beyond my expectations.",
-    stars: 4,
+    text: "Very good quality-price ratio. Jude is fair and his team are magicians of the web! Great team to work with. They've worked hard to pull off a fantastic result beyond my expectations. Excellent work ethic and engagement. Highly recommend their services.",
+    stars: 5,
+  },
+  {
+    username: "Ayush Kokra",
+    text: "Crezeal helped me with the best customization of the website alongside with the cooperative team. The developers know what they are doing, I am very satisfied with my website as the developers are also knowledgeable about work and the field. I got no chance of complaints against them.",
+    stars: 5,
   },
 ];
 
